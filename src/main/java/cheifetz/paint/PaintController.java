41783e8715aa81136859c3de5c0a5b96431e6c93
ahuvac <58051262+ahuvac@users.noreply.github.com>
2021-04-11 19:52:20 -0400

    fixed variables

--- src/main/java/cheifetz/paint/PaintController.java
+++ src/main/java/cheifetz/paint/PaintController.java
@@ -4,17 +4,9 @@ package cheifetz.paint;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-
-
-import java.awt.*;
 
 
 public class PaintController {
@@ -22,7 +14,8 @@ public class PaintController {
     @FXML
     private PaintCanvas paintCanvas;
     @FXML
-    private ColorPicker colorpicker;
+    private ColorPicker colorPicker
+            ;
     @FXML
     private Slider slider;
 
@@ -37,11 +30,11 @@ public class PaintController {
                 paintCanvas.setLineWidth(new_val.doubleValue());
             }
         });
-        colorpicker.setValue(Color.BLACK);
+        colorPicker.setValue(Color.BLACK);
     }
 
     public void changeColor(ActionEvent actionEvent) {
-        paintCanvas.setColor(colorpicker.getValue());
+        paintCanvas.setColor(colorPicker.getValue());
     }
 
     public void clear(){
